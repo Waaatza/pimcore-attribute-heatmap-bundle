5c90a997f0359943706ceeef4cf017b6ada91d72
Henri Faust <faust.henri@outlook.de>
2026-09-19 04:32:51 +0200

    Improve heatmap result tiles layout and rebuild frontend

--- public/build/4f46ed40-6e01-46c9-bc6f-626a29104daf/exposeRemote.js
+++ public/build/4f46ed40-6e01-46c9-bc6f-626a29104daf/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.attribute_heatmap_bundle = "/bundles/pimcoreattributeheatmap/build/aca2153d-8c24-4127-91bb-63b95ea6bc2e/static/js/remoteEntry.js"
+      window.pluginRemotes.attribute_heatmap_bundle = "/bundles/pimcoreattributeheatmap/build/4f46ed40-6e01-46c9-bc6f-626a29104daf/static/js/remoteEntry.js"
 
       
     
